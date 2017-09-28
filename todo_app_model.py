deb50a2bdfb7a62c4a097e0439d2039995c8b16c
bertokpeter <bertokpeter92@gmail.com>
2017-09-28 13:57:36 +0200

    refact(todo_model): rewritten file path

--- todo_app_model.py
+++ todo_app_model.py
@@ -1,7 +1,7 @@
 class TodoModel():
 
-    def dictionary(self):
+    def __init__(self):
         self.todo_list = []
-        with open("bertokpeter-todo-app/todos.txt", "r") as self.todo_file:
+        with open("todos.txt", "r") as self.todo_file:
             for line in self.todo_file:
-                self.todo_list.append({"checked": False, "name": line.rstrip("\n")})      
+                self.todo_list.append({"checked": False, "name": line.rstrip("\n")})   
