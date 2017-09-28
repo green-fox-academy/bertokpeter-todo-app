d621ba122eda5f1d997331bcf8be1bc34b4244a9
bertokpeter <bertokpeter92@gmail.com>
2017-09-28 11:54:07 +0200

    feat(todo_view): print usage method

---
+++ todo_app_view.py
@@ -0,0 +1,12 @@
+class TodoView():
+
+    def print_usage(self):
+        print("\n\
+               Command Line Todo application\n\
+               =============================\n\
+               \n\
+               Command line arguments:\n\
+               -l   Lists all the tasks\n\
+               -a   Adds a new task\n\
+               -r   Removes an task\n\
+               -c   Completes an task")
