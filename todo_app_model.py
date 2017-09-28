81cd3ea873e709f582b0ba5bd8eb1de08ba237cc
bertokpeter <bertokpeter92@gmail.com>
2017-09-28 16:10:12 +0200

    feat(toso_model)write method

--- todo_app_model.py
+++ todo_app_model.py
@@ -2,6 +2,12 @@ class TodoModel():
 
     def __init__(self):
         self.todo_list = []
-        with open("todos.txt", "r") as self.todo_file:
-            for line in self.todo_file:
-                self.todo_list.append({"checked": line[0] == "1", "name": line[2:].rstrip("\n")})                    
+        with open("todos.txt", "r") as self.file:
+            for line in self.file:
+                self.todo_list.append({"checked": line[0] == "1", "name": line[2:].rstrip("\n")})
+    
+    def write(self):
+        with open("todos.txt", "w") as self.file:
+            for element in self.todo_list:
+                one = "1" if element["checked"] else "0"
+                self.file.write(one + " " + element["name"] + "\n")
