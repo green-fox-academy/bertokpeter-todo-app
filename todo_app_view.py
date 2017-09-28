954bd1d49af78c0e2bd8048a215febf4c0141ee4
bertokpeter <bertokpeter92@gmail.com>
2017-09-28 22:21:17 +0200

    feat(todo_view):lists undone tasks option

--- todo_app_view.py
+++ todo_app_view.py
@@ -6,19 +6,24 @@ class TodoView():
                =============================\n\
                \n\
                Command line arguments:\n\
-               -l   Lists all the tasks\n\
-               -a   Adds a new task\n\
-               -r   Removes an task\n\
-               -c   Completes an task")
+               -l    Lists undone tasks\n\
+               -la   Lists all the tasks\n\
+               -a    Adds a new task\n\
+               -r    Removes an task\n\
+               -c    Completes an task")
 
-    def print_list(self, dict_list):
+    def print_list(self, dict_list, list_all):
+        if not list_all:
+            for i in dict_list:
+                if i["checked"]:
+                    dict_list.remove(i)
         if len(dict_list) == 0:
             print("No todos for today! :)")
         for i in range(len(dict_list)):
             task = dict_list[i]["name"]
             check = "x" if dict_list[i]["checked"] else " "
             print(str(i+1) + " - [" + check + "] " + task)
-
+    
     def arg_error(self):
         print("Unsupported argument")
         self.print_usage()
