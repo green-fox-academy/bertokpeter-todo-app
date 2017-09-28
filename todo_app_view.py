647b14a367215ce6845534fb1ff4ece93df836fd
bertokpeter <bertokpeter92@gmail.com>
2017-09-29 00:02:31 +0200

    refact(todo_view):prints undone list with correct numbers

--- todo_app_view.py
+++ todo_app_view.py
@@ -13,15 +13,19 @@ class TodoView():
                -c    Completes an task")
 
     def print_list(self, dict_list, list_all):
+        original_list = dict_list
         if not list_all:
             dict_list = [elem for elem in dict_list if not elem["checked"]]
         if len(dict_list) == 0:
             print("No todos for today! :)")
-        for i in range(len(dict_list)):
-            task = dict_list[i]["name"]
-            check = "x" if dict_list[i]["checked"] else " "
-            print(str(i+1) + " - [" + check + "] " + task)
-    
+        for i in range(len(original_list)):
+            task = original_list[i]["name"]
+            check = "x" if original_list[i]["checked"] else " "
+            if list_all:
+                print(str(i+1) + " - [" + check + "] " + task)
+            elif not original_list[i]["checked"]:
+                print(str(i+1) + " - [ ] " + task)
+                
     def arg_error(self):
         print("Unsupported argument")
         self.print_usage()
